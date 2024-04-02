82486b8cf2e2e5bccde97476c94fc4ba382628b2
Malachi Asgharian <malasgharian@gmail.com>
2024-04-02 13:22:34 +0100

    first commit

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+
+    validates :title, presence: true
+
+    after_create_commit {broadcast_prepend_to "posts"}
+    after_destroy_commit {broadcast_remove_to "posts"}
+end
